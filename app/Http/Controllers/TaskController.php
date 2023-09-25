4271d19ea70bb9fb23db734fc830b959939963c7
Keerthy007 <keerthy@udyata.com>
2023-09-25 14:22:49 +0530

    Updated

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Task;
+use App\Models\User;
 use Validator;
 
 use Illuminate\Http\Request;
@@ -35,7 +36,7 @@ class TaskController extends Controller
         $validator = Validator::make($request->all(),[
             'task_name' => 'required',
             'task_desc' => 'required',
-            'created_by' => 'required',
+            'user_id' => 'required',
             'token' => 'required',
         ]);
 
@@ -43,22 +44,31 @@ class TaskController extends Controller
             $msg = $validator->messages()->first();
             return response()->json(['response_code' => false, 'message' => $msg]);
         }
-
-        $task_name = $request->task_name;
-        $task_desc = $request->task_desc;
-        $created_by = $request->created_by;
-        $task_level = $request->task_level ? $request->task_level:null;
-        $is_active = 1;
-        $time_limit = $request->time_limit ? $request->time_limit:null;
-        $task_date = $request->task_date ? $request->task_date:null;
-        $difficulty_level = $request->difficulty_level ? $request->difficulty_level:null;
-        $save_template = $request->save_template ? $request->save_template:null;
-        $token = $request->token;
+        
+     $user_id = $request->user_id;
+     $user_access_token  = $request->token;
+     $TokenCheck = User::where('id', $user_id)->first();
+     $DB_token = $TokenCheck->remember_token;
+     if($DB_token == $user_access_token){
+         
+        $Role_db = User::where('id', $user_id)->get('role');
+        $roleCheck = $Role_db[0]->role;
+        if($roleCheck == 1) {
+         $task_name = $request->task_name;
+          $task_desc = $request->task_desc;
+     //   $created_by = $request->created_by;
+          $task_level = $request->task_level ? $request->task_level:null;
+          $is_active = 1;
+          $time_limit = $request->time_limit ? $request->time_limit:null;
+          $task_date = $request->task_date ? $request->task_date:null;
+          $difficulty_level = $request->difficulty_level ? $request->difficulty_level:null;
+          $save_template = $request->save_template ? $request->save_template:null;
+          //$token = $request->token;
 
         $result = Task::insert([
             'task_name' => $task_name,
             'task_desc' => $task_desc,
-            'created_by' => $created_by,
+            'created_by' => $user_id,
             'task_level' => $task_level,
             'is_active' => $is_active,
             'time_limit' => $time_limit,
@@ -68,11 +78,17 @@ class TaskController extends Controller
             'created_date' => date('Y-m-d')
         ]);
 
-        if($result == true){
-            return response()->json(['status' => true, 'message' => "Task added successfully"]);
-        } else {
-            return response()->json(['status' => false, ' message' => "Failed to add task"]);
-        }
+            if($result == true){
+                return response()->json(['status' => true, 'message' => "Task added successfully"]);
+             } else {
+                return response()->json(['status' => false, ' message' => "Failed to add task"]);
+             }
+         } else {
+             return respose()->json(['status' => false, 'message' => "Access denied"]);
+         }
+       } else {
+          return response()->json(['status' => false, 'message' => "Invalid token"]);
+      }
     }
 
     ///Update Task
@@ -88,21 +104,28 @@ class TaskController extends Controller
             $msg = $validator->messages()->first();
             return response()->json(['response_code' => false, 'message' => $msg]);
         }
+        $user_id = $request->updated_by;
+        $user_access_token  = $request->token;
+        $TokenCheck = User::where('id', $user_id)->first();
+        $DB_token = $TokenCheck->remember_token;                                                                  
+        if($DB_token == $user_access_token){
 
-        $task_name = $request->task_name;
-
-        $TaskCheck = Task::select('*')->where('task_name', $task_name)->first();
-       
-        if($TaskCheck != null){
-            $task_desc = $request->task_desc ? $request->task_desc:$TaskCheck[0]->task_desc;
-            $updated_by = $request->updated_by;
-            $task_level = $request->task_level ? $request->task_level:$TaskCheck[0]->task_level;
-            $is_active = 1;
-            $time_limit = $request->time_limit ? $request->time_limit:$TaskCheck[0]->time_limit;
-            $task_date = $request->task_date ? $request->task_date:$TaskCheck[0]->task_date;
-            $difficulty_level = $request->difficulty_level ? $request->difficulty_level:$TaskCheck[0]->difficulty_level;
-            $save_template = $request->save_template ? $request->save_template:$TaskCheck[0]->save_template;
-            $token = $request->token;
+            $Role_db = User::where('id', $user_id)->get('role');
+            $roleCheck = $Role_db[0]->role;
+            if($roleCheck == 1) {
+                $task_name = $request->task_name;
+                $TaskCheck = Task::select('*')->where('task_name', $task_name)->first();
+         
+                if($TaskCheck != null){
+                    $task_desc = $request->task_desc ? $request->task_desc:$TaskCheck[0]->task_desc;
+                    $updated_by = $request->updated_by;
+                    $task_level = $request->task_level ? $request->task_level:$TaskCheck[0]->task_level;
+                    $is_active = 1;
+                    $time_limit = $request->time_limit ? $request->time_limit:$TaskCheck[0]->time_limit;
+                    $task_date = $request->task_date ? $request->task_date:$TaskCheck[0]->task_date;
+                    $difficulty_level = $request->difficulty_level ? $request->difficulty_level:$TaskCheck[0]->difficulty_level;
+                    $save_template = $request->save_template ? $request->save_template:$TaskCheck[0]->save_template;
+                //$token = $request->token;
     
             $result = Task::where('task_name', $task_name)->update([
                 'task_name' => $task_name,
@@ -123,10 +146,14 @@ class TaskController extends Controller
             }
         } else{
             return response()->json(['status' => false, 'message' => "No such task found"]);
-        }
-        
+        } 
+      } else {
+         return response()->json(['status' => false, 'message' => "Access denied"]);
+      }
+    }else {
+        return response()->json(['status'=>false, 'message'=>"Invalid token"]);
     }
-
+ }
     //Get tasks
     public function GetTask(){
         
@@ -170,7 +197,7 @@ class TaskController extends Controller
             $msg = $validator->messages()->first();
             return response()->json(['response_code' => false, 'message' => $msg]);
         }
-
+        $task_name = $request->task_name;
         $result = Task::where('task_name', $task_name)->delete();
 
         if($result == true){
